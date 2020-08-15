24e788d88e26ca5d139fbe27079dddb5add5a507
StephenPopovich <stephenpopovich16@gmail.com>
2020-08-15 11:57:43 -0700

    update title and intro of homepage

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React App Guide</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is one of my first React applications
         </p>
       </div>
     );
