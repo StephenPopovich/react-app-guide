b12a3b84115fb5aa36087c2ed547ef267eff6533
StephenPopovich <stephenpopovich16@gmail.com>
2020-08-15 17:27:54 -0700

    create person component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <h1 className="App-title">React App Guide</h1>
-    //     </header>
-    //     <p className="App-intro">
-    //       This is one of my first React applications
-    //     </p>
-    //   </div>
-    // );
-    return React.createElement('div', null, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React App Guide</h1>
+        </header>
+        <p className="App-intro">
+          This is one of my first React applications
+        </p>
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', null, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+   return <p>I'm a Component called person!</p>
+}
+
+export default person;
