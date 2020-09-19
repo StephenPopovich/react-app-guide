9b0d2f6a2737bc7128f223443e0a3d2f01f69324
StephenPopovich <stephenpopovich16@gmail.com>
2020-09-18 19:50:38 -0700

    transform component to class based component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
 
-// new component that is starting with an uppercase inorder to use this component in the same file.
+// class based new component that is starting with an uppercase inorder to use this component in the same file.
 //function
-const person = (props) => {
-  console.log('[Person.js] rendering...');
+class Person extends Component {
+  render(){
+    console.log('[Person.js] rendering...');
+
    return (
         <div className={classes.Person}>
-          <p onClick={props.click}>I'm a {props.name} I am {props.age} years old!</p>
-          <p>{props.children}</p>
-          <input type="text" onChange={props.changed}  value={props.name}/>
+          <p onClick={this.props.click}>I'm a {this.props.name} I am {this.props.age} years old!</p>
+          <p>{this.props.children}</p>
+          <input type="text" onChange={this.props.changed}  value={this.props.name}/>
         </div>
- )
+  );
+ }
 };
 
-export default person;
+export default Person;
